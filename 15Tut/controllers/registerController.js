e1841047104b2bd8436f98cfd81b96bb5d6ca3a0
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-08-04 23:55:38 +0100

    Updated CRUD APIs

--- 15Tut/controllers/registerController.js
+++ 15Tut/controllers/registerController.js
@@ -8,9 +8,11 @@ const bcrypt = require('bcrypt');
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+    
     // check for duplicate usernames in the db
     const duplicate = await User.findOne({ username: user}).exec();
     if (duplicate) return res.sendStatus(409); //Conflict 
+    
     try {
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
