adce6370235fa44fa2ef83192fd0e1006e803ba4
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-02 17:11:23 +0100

    Array functions

--- JSTuts/JS15/js/moreArrays.js
+++ JSTuts/JS15/js/moreArrays.js
@@ -3,3 +3,9 @@ const myArray = ["A", "B", "C", "D", "E", "F"]
 // New Array returns values from index 2
 const newArray = myArray.slice(2)
 console.log(newArray)
+
+// Array methods include
+    // .splice() - helps to delete and return
+    // .reverse() - revesres the order of an array
+    // .join() Creates an new string of an array
+    // This string created can be returned back to an Array using th split() method
