11e8032c2c151719066e7a8d2d385430ca30b104
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-02 10:36:13 +0100

    any

---
+++ JSTuts/JS11/js/main.js
@@ -0,0 +1,5 @@
+// console.log("Hello");
+// let myBoolean = confirm("Ok === True\nCancel === False")
+// console.log(myBoolean);
+const fname = prompt("Please enter your name");
+console.log(fname ?? "You didn't enter your name");
