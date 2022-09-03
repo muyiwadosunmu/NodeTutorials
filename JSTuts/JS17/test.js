f0fefa1baa13d41dcd23fb5dcb85da45282a95a7
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-03 01:48:30 +0100

    More examples on classes

--- JSTuts/JS17/test.js
+++ JSTuts/JS17/test.js
@@ -1,16 +1,26 @@
 // JavaScript Classes
-class Pizza {
-    constructor(pizzaSize) {
-        this._size = pizzaSize;
-        this._crust = "original";
+    // Using Factory functions
+function pizzaFactory(pizzaSize) {
+    const crust = "original";
+    const size = pizzaSize;
+    return {
+        bake: () => console.log(`Baking a ${size} ${crust} crust pizza `)
     }
-    getCrust() {
-        return this.crust;
-    }
-    setCrust(pizzaCrust) {
-        this.crust = pizzaCrust
-    }
-} 
+}const myPizza = pizzaFactory("small");
+myPizza.bake();
+
+// class Pizza {
+//     constructor(pizzaSize) {
+//         this._size = pizzaSize;
+//         this._crust = "original";
+//     }
+//     getCrust() {
+//         return this.crust;
+//     }
+//     setCrust(pizzaCrust) {
+//         this.crust = pizzaCrust
+//     }
+// } 
 
 // For naming conventions we could refactor our codes to tell other
 // developers that we intend to make the properties above private
