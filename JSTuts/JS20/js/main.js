f325d99758de6e1849ba88f03a1907faddad77c1
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-04 23:03:12 +0100

    Starting out with DOM

--- JSTuts/JS20/js/main.js
+++ JSTuts/JS20/js/main.js
@@ -5,11 +5,14 @@ const view1 = document.getElementById("view1");
 console.log(view1)
 
 // Method 2
-const view2 = document.querySelector("view2");
+const view2 = document.querySelector("#view2");
 console.log(view2);
 
 view1.style.display = "flex";
+
 view2.style.display = "none";
 
 const views = document.getElementsByClassName("view");
 console.log(views)
+
+const sameViews = document.querySelectorAll(".view")
