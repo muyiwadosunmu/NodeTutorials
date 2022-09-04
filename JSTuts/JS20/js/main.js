c8b32ce4b250849929d6dd7b29008c3e705c6afb
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-04 20:25:32 +0100

    Starting out with DOM

--- JSTuts/JS20/js/main.js
+++ JSTuts/JS20/js/main.js
@@ -1,10 +1,15 @@
 // DOM - Document Object Model
 
+// Methode1
 const view1 = document.getElementById("view1");
 console.log(view1)
 
+// Method 2
 const view2 = document.querySelector("view2");
 console.log(view2);
 
 view1.style.display = "flex";
 view2.style.display = "none";
+
+const views = document.getElementsByClassName("view");
+console.log(views)
