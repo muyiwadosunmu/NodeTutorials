41fac5cb47fe9a02ac07954401aba8fb600885f7
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-05 03:16:10 +0100

    Event Listeners

--- JSTuts/JS21/js/events.js
+++ JSTuts/JS21/js/events.js
@@ -5,3 +5,14 @@ const div = view.querySelector("div");
 const h2 = div.querySelector("h2");
 
 // Syntax: addEventListener(event, function, useCapture)
+const doSomething = () => {
+    alert("Doing Something");
+}
+h2.addEventListener("click", doSomething, false);
+h2.removeEventLIstener("click", doSomething. false);
+
+
+h2.addEventListener("click", (event) => {
+    console.log(event.target);
+    event.target.textContent = "Clicked"
+})
